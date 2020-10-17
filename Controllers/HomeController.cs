b8a3af965158e5868d16b5867c085084d813d5b3
Xavier Poinas <xavier.poinas@gmail.com>
2020-10-17 12:12:20 +0300

    Setup attribute routing with {lang} parameter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace Repro.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("", Name = "Index")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("about-us", Name = "About")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,11 +22,20 @@ namespace Repro.Controllers
             return View();
         }
 
+        [Route("contact", Name = "Contact")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext ctx)
+        {
+            // Get the current language code from the URL
+            ViewBag.Lang = (string)ctx.RouteData.Values["lang"];
+
+            base.OnActionExecuting(ctx);
         }
     }
 }
